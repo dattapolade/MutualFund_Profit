e8f3ed14c3f0edaad23b8ba8ede5769171092492
dattapolade <dattatraybpolade1506@gmail.com>
2024-05-13 19:59:06 +0530

    Adding the mutual fund profit code

---
+++ fronted/script.js
@@ -0,0 +1,30 @@
+async function calculateProfit() {
+    const schemeCode = document.getElementById('scheme_code').value;
+    const startDate = document.getElementById('start_date').value;
+    const endDate = document.getElementById('end_date').value;
+    const capital = parseFloat(document.getElementById('capital').value);
+
+    try {
+        const response = await fetch(`http://localhost:8000/profit?scheme_code=${schemeCode}&start_date=${startDate}&end_date=${endDate}&capital=${capital}`, {
+            method: 'GET',
+            mode: 'cors'
+        });
+
+        
+        if (response.ok) {
+
+        const data = await response.json();
+        document.getElementById('profit-value').innerText = data.profit;
+        document.getElementById('success-message').style.display = 'block';
+        document.getElementById('error-message').style.display = 'none';
+       } else {
+        throw new Error('Request failed');
+    }
+
+    } catch (error) {
+        console.error('Error:', error);
+        document.getElementById('error-text').innerText = 'An error occurred. Please try again.';
+        document.getElementById('error-message').style.display = 'block';
+        document.getElementById('success-message').style.display = 'none';
+    }
+}
